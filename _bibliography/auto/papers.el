345c718f5e281e1fbfe027f6be9ef6d41c002810
Bernardo Almeida <bernardo.pinto.almeida@gmail.com>
2024-09-18 15:26:45 +0100

    tuning papers

--- _bibliography/auto/papers.el
+++ _bibliography/auto/papers.el
@@ -7,6 +7,8 @@
     "DBLP:conf/tacas/AlmeidaMV20"
     "DBLP:conf/esop/VasconcelosCAM20"
     "DBLP:journals/corr/abs-1904-01284"
+    "inforum19"
+    "inforum18"
     "almeida2018linguagem"))
  '(or :bibtex :latex))
 
