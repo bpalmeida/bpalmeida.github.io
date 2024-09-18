afe1500450fcfc2f9b2a274660aad3a6d2d18345
Bernardo Almeida <bernardo.pinto.almeida@gmail.com>
2024-09-18 14:58:48 +0100

    adding papers

--- _bibliography/auto/papers.el
+++ _bibliography/auto/papers.el
@@ -4,6 +4,8 @@
    (LaTeX-add-bibitems
     "DBLP:journals/corr/abs-2304-06396"
     "ALMEIDA2022104948"
+    "AlmeidaDS22"
+    "inforum22"
     "DBLP:conf/tacas/AlmeidaMV20"
     "DBLP:conf/esop/VasconcelosCAM20"
     "DBLP:journals/corr/abs-1904-01284"
